32407512893cb6ac16a1fa3c296e134d38450239
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-10-02 11:50:08 +0200

    made constant for number od problems

--- src/optional/NinetyNineProblems.java
+++ src/optional/NinetyNineProblems.java
@@ -3,8 +3,10 @@ package optional;
 /* Use the Extract Constant refactoring to remove the magic number. */
 
 public class NinetyNineProblems {
+	private static final int NUMBER_OF_PROBLEMS = 1000;
+
 	public static void main(String[] args) {
-		String lyrics = "If you're having girl problems I feel bad for you, son. I got " + 99 + " problems but ugly code ain't one.";
+		String lyrics = "If you're having girl problems I feel bad for you, son. I got " + NUMBER_OF_PROBLEMS + " problems but ugly code ain't one.";
 		System.out.println(lyrics);
 	}
 }
