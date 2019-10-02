14b9dbfae29dd8975a735c6ecdbfc35185db886a
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-10-02 11:53:31 +0200

    new class for WorkIt

---
+++ src/optional/PutMyThingDownParameter.java
@@ -0,0 +1,20 @@
+package optional;
+
+import java.awt.Color;
+
+public class PutMyThingDownParameter {
+	public int missySneakerSize;
+	public Color missyCapColor;
+	public String bling;
+	public int numberOfGrammyAwards;
+	public boolean isSupaDupaFly;
+
+	public PutMyThingDownParameter(int missySneakerSize, Color missyCapColor, String bling, int numberOfGrammyAwards,
+			boolean isSupaDupaFly) {
+		this.missySneakerSize = missySneakerSize;
+		this.missyCapColor = missyCapColor;
+		this.bling = bling;
+		this.numberOfGrammyAwards = numberOfGrammyAwards;
+		this.isSupaDupaFly = isSupaDupaFly;
+	}
+}
